c34734fafbd185e23a84731f261c75af249842e3
suyeonh <suyeonh12@gmail.com>
2025-07-24 15:09:40 +0900

    [add] 확인하지 않은 알림 리스트  컴포넌트 생성

--- front/src/pages/MainFarm.jsx
+++ front/src/pages/MainFarm.jsx
@@ -1,6 +1,7 @@
 // src/pages/MainFarm.jsx
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
+import AlertList from '../components/AlertList';
 
 export default function MainFarm() {
   const { user } = useAuth();
@@ -11,9 +12,7 @@ export default function MainFarm() {
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">{farm.name} 관리 페이지</h2>
-      <p>농장 ID: {farm.id}</p>
-      {/* 추가적인 농장 정보 출력 또는 관리 기능 */}
+      <AlertList/>
     </div>
   );
 }
