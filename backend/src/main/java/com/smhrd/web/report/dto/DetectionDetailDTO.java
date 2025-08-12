9cd3e174c9d4abbc203a887d04a740e0384e72a7
yeonjinju <wnduswls21@gmail.com>
2025-08-13 08:59:45 +0900

    일별상세 수정

--- backend/src/main/java/com/smhrd/web/report/dto/DetectionDetailDTO.java
+++ backend/src/main/java/com/smhrd/web/report/dto/DetectionDetailDTO.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class DetectionDetailDTO {
-	// 상세 리스트 (시간, 해충명, 정확도 등)
+   // 상세 리스트 (시간, 해충명, 정확도 등)
+   private int anlsIdx;
     private String time;
     private String greenhouse;
     private String insect;
